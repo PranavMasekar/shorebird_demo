e2d1f962b8c9f1c665c4c182ae7bfca9f47fa336
PranavMasekar <pranavmasekar4@gmail.com>
2024-11-17 12:59:00 +0530

    Patch #2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Shorebird Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Shorebird Demo'),
